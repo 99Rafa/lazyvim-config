f096f2de4775f5a3b2693338714df7e8cb1fab31
Rafael Aguirre <rafael.aguirre.ro@gmail.com>
2024-03-12 00:57:27 -0600

    Change file_browser layout to vertical

--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -4,12 +4,36 @@ return {
   keys = {
     {
       "<leader>fo",
-      "<cmd>Telescope file_browser<cr>",
+      --"<cmd>Telescope file_browser<cr>",
+      function()
+        require("telescope").extensions.file_browser.file_browser({
+          layout_strategy = "vertical",
+          layout_config = {
+            vertical = {
+              prompt_position = "top",
+              mirror = true,
+            },
+          },
+        })
+      end,
       desc = "File browser (cwd)",
     },
     {
       "<leader>fO",
-      ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      --"<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      function()
+        require("telescope").extensions.file_browser.file_browser({
+          path = "%:p:h",
+          select_buffer = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            vertical = {
+              prompt_position = "top",
+              mirror = true,
+            },
+          },
+        })
+      end,
       desc = "File browser (root)",
     },
   },
