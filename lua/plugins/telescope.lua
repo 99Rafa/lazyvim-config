29f657a4695f55b9681748d8595bcc555bed159d
Rafael Aguirre <rafael.aguirre.ro@gmail.com>
2024-03-12 00:08:38 -0600

    Change notify layout to vertical

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,6 +16,25 @@ return {
 
   keys = {
     { "<leader>fw", require("lazyvim.util").telescope("live_grep"), desc = "Grep (root dir)" },
-    { "<leader>fn", "<cmd>Telescope notify<cr>", desc = "Grep (root dir)" },
+    -- {
+    --   "<leader>fn",
+    --   "<cmd>Telescope notify layout_strategy=vertical layout_config={vertical={preview_height=0.7}}<cr>",
+    --   desc = "Grep (root dir)",
+    -- },
+    {
+      "<leader>fn",
+      function()
+        require("telescope").extensions.notify.notify({
+          layout_strategy = "vertical",
+          layout_config = {
+            vertical = {
+              prompt_position = "top",
+              preview_height = 0.7,
+            },
+          },
+        })
+      end,
+      desc = "Notify history",
+    },
   },
 }
