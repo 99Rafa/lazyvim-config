6f3fe339b302a6789bbab28222703af7c609e82c
Rafael Aguirre <rafael.aguirre.ro@gmail.com>
2024-03-19 16:29:43 -0600

    Changed lualine_a to be a single letter

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,13 @@
+local node_map = {
+  n = "N",
+  i = "I",
+  c = "C",
+  V = "VL",
+  [""] = "VB",
+  v = "V",
+  t = "T",
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -6,6 +16,17 @@ return {
       section_separators = { left = "", right = "" },
     },
     sections = {
+      lualine_a = {
+        function()
+          local mode = vim.fn.mode()
+
+          if node_map[mode] ~= nil then
+            return node_map[mode]
+          end
+
+          return mode
+        end,
+      },
       lualine_z = {},
     },
   },
