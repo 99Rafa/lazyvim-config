116441392fa3c6bef74fc3f372249ed586b810a3
99Rafa <color/gh@slmail.me>
2025-09-13 23:43:18 -0600

    Change lazygit border color

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,14 @@
 return {
   "snacks.nvim",
+  init = function()
+    vim.api.nvim_set_hl(0, "LazygitInactiveBorder", { bg = "#414868" })
+  end,
   opts = {
     scroll = { enabled = false },
+    lazygit = {
+      theme = {
+        inactiveBorderColor = { bg = "LazygitInactiveBorder" },
+      },
+    },
   },
 }
